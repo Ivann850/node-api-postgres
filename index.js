3dd6520489d59431a699d0b54b8729040fb60bff
Ivann Alcantara <ivann850@gmail.com>
2025-03-23 15:55:04 -0400

    fixed running issue

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.listen(port, () => {
 })
 
 app.get('/users', db.getUsers);
-app.get('/users/:id', db.getUsersById);
+app.get('/users/:id', db.getUserById);
 app.post('/users', db.createUser);
 app.put('/users/:id', db.updateUser);
 app.delete('/users/:id', db.deleteUser);
